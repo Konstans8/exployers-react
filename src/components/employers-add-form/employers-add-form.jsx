95b44b048c567e97688c4ed67b8ede1babd914a3
konstans8 <kostian73kostian@gmail.com>
2022-10-10 22:10:27 +0200

    react-app

---
+++ src/components/employers-add-form/employers-add-form.jsx
@@ -0,0 +1,24 @@
+import './employers-add-form.css';
+
+const EmployersAddForm = () => {
+    return (
+        <div className="app-add-form">
+            <h3>Добавте нового сотрудника</h3>
+            <form 
+                className="add-form d-flex">
+                <input 
+                    type="text" 
+                    className="form-control new-post-label"
+                    placeholder="Как его зовут?"/>
+                <input 
+                    type="text" 
+                    className="form-control new-post-lable"
+                    placeholder="З/П в $?"/>
+                    
+                <button type="submit" className="btn btn-outline-light">Добавить</button>
+            </form>
+        </div>
+    )
+}
+
+export default EmployersAddForm;
