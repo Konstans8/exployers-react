8dd36caacc3e945762196c8024c05bc0234bdbfe
konstans8 <kostian73kostian@gmail.com>
2022-10-14 22:32:16 +0200

    modification

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,41 +3,39 @@ import { Component } from 'react';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
-import EmployersList from '../employers-list/employers-list';
-import EmployersAddForm from '../employers-add-form/employers-add-form';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
 class App extends Component {
-    
     constructor(props) {
         super(props);
         this.state = {
             data: [
-                {name: 'John C.', salary: 800, increase: false, id: 1},
-                {name: 'Alex M.', salary: 3000, increase: true, id: 2},
-                {name: 'Mike W.', salary: 15000, increase: false, id: 3},
-                {name: 'Kate E.', salary: 8000, increase: false, id: 4},
+                {name: 'John C.', salary: 800, increase: false, rise: true, id: 1},
+                {name: 'Alex M.', salary: 3000, increase: true, rise: false, id: 2},
+                {name: 'Carl W.', salary: 5000, increase: false, rise: false, id: 3}
             ]
         }
         this.maxId = 4;
     }
-    
+
     deleteItem = (id) => {
         this.setState(({data}) => {
-            const index = data.findIndex(elem => elem.id === id);
-            
             return {
-                data: data.filter(item => item.id !== id) 
+                data: data.filter(item => item.id !== id)
             }
         })
     }
-    
+
+    // Да, пока могут добавляться пустые пользователи. Мы это еще исправим
     addItem = (name, salary) => {
         const newItem = {
-            name,
+            name, 
             salary,
             increase: false,
+            rise: false,
             id: this.maxId++
         }
         this.setState(({data}) => {
@@ -47,26 +45,38 @@ class App extends Component {
             }
         });
     }
-    
+
+    onToggleProp = (id, prop) => {
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, [prop]: !item[prop]}
+                }
+                return item;
+            })
+        }))
+    }
+
     render() {
+        const employees = this.state.data.length;
+        const increased = this.state.data.filter(item => item.increase).length;
         return (
             <div className="app">
-                <AppInfo />
-                
+                <AppInfo employees={employees} increased={increased}/>
+    
                 <div className="search-panel">
-                    <SearchPanel />
-                    <AppFilter />
+                    <SearchPanel/>
+                    <AppFilter/>
                 </div>
                 
-                <EmployersList 
+                <EmployeesList 
                     data={this.state.data}
-                    onDelete={this.deleteItem}/>
-                <EmployersAddForm 
-                    onAdd={this.addItem}/>
+                    onDelete={this.deleteItem}
+                    onToggleProp={this.onToggleProp}/>
+                <EmployeesAddForm onAdd={this.addItem}/>
             </div>
-        )
+        );
     }
-    
 }
 
 export default App;
--- src/components/employees-add-form/employees-add-form.jsx
+++ src/components/employees-add-form/employees-add-form.jsx
@@ -1,9 +1,10 @@
-import { Component} from 'react';
-
-import './employers-add-form.css';
 
 
-class EmployersAddForm extends Component {
+import { Component } from 'react';
+
+import './employees-add-form.css';
+
+class EmployeesAddForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -11,15 +12,17 @@ class EmployersAddForm extends Component {
             salary: ''
         }
     }
-    
+
     onValueChange = (e) => {
         this.setState({
             [e.target.name] : e.target.value
         })
     }
-    
+
     onSubmit = (e) => {
         e.preventDefault();
+        // Можно еще и сообщения добавлять, подсветку, атрибуты minlength и тд.
+        if (this.state.name.length < 3 || !this.state.salary) return;
         this.props.onAdd(this.state.name, this.state.salary);
         this.setState({
             name: '',
@@ -29,33 +32,32 @@ class EmployersAddForm extends Component {
 
     render() {
         const {name, salary} = this.state;
-    
+
         return (
             <div className="app-add-form">
-                <h3>Добавте нового сотрудника</h3>
-                <form 
+                <h3>Добавьте нового сотрудника</h3>
+                <form
                     className="add-form d-flex"
-                    onSubmit={this.onSubmit}>
-                    <input 
-                        type="text" 
+                    onSubmit = {this.onSubmit}>
+                    <input type="text"
                         className="form-control new-post-label"
                         placeholder="Как его зовут?"
-                        name='name'
-                        value={name}
+                        name="name"
+                        value={name} 
                         onChange={this.onValueChange}/>
-                    <input 
-                        type="text" 
-                        className="form-control new-post-lable"
+                    <input type="number"
+                        className="form-control new-post-label"
                         placeholder="З/П в $?"
-                        name='salary'
-                        value={salary}
+                        name="salary"
+                        value={salary} 
                         onChange={this.onValueChange}/>
-                        
-                    <button type="submit" className="btn btn-outline-light">Добавить</button>
+    
+                    <button type="submit"
+                            className="btn btn-outline-light">Добавить</button>
                 </form>
             </div>
         )
     }
 }
 
-export default EmployersAddForm;
+export default EmployeesAddForm;
