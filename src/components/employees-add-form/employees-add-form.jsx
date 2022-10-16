375d807d745e17c4a2e7a3e980c92deb6deb42c3
konstans8 <kostian73kostian@gmail.com>
2022-10-16 11:22:34 +0200

    employees-app

--- src/components/employees-add-form/employees-add-form.jsx
+++ src/components/employees-add-form/employees-add-form.jsx
@@ -2,7 +2,8 @@
 
 import { Component } from 'react';
 
-import './employees-add-form.css';
+// import './employees-add-form.css';
+import './employees-add-form.scss';
 
 class EmployeesAddForm extends Component {
     constructor(props) {
@@ -21,7 +22,6 @@ class EmployeesAddForm extends Component {
 
     onSubmit = (e) => {
         e.preventDefault();
-        // Можно еще и сообщения добавлять, подсветку, атрибуты minlength и тд.
         if (this.state.name.length < 3 || !this.state.salary) return;
         this.props.onAdd(this.state.name, this.state.salary);
         this.setState({
